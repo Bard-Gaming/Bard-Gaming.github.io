63414e26f7f004ec92493eb6fc334b53e6d72cce
Bard-Gaming <christophe@dronne.fr>
2023-01-23 19:00:40 +0100

    added some stuff, fixed flash cards

--- javaScript/flashCards.js
+++ javaScript/flashCards.js
@@ -1,20 +1,34 @@
-var charList = ["无聊", "尴尬", "告诉", "打算", "参观", "胃口好", "感冒", "反过来说", "春夏秋冬", "虽然", "桌子", "开始"]; //set principal, change lorsqu'on choisit un autre ensemble.
-var pinyinList = ["wu liao", "gan ga", "gao su", "da suan", "can guan", "wei kou hao", "gan mao", "fan guo lai shuo", "chun xia qiu dong", "sui ran", "zhuo zi", "kai shi"];
-var definitionList = ["ennuyeux", "génant", "avertir/prévenir", "planifier", "visiter", "bon appétit!", "rhume", "d'autre part (connecteur logique)", "printemps, été, automne, hiver", "cependant (connecteur logique)", "table", 'Commencer (appuyez sur "Prochaine Carte")']
+//-----------------------------------------------Constantes-------------------------------------------------------------:
+//Character Lists:
+var randomSet = ["无聊", "尴尬", "告诉", "打算", "参观", "胃口好", "感冒", "反过来说", "春夏秋冬", "虽然", "桌子", "开始"];
+const secondSet = ["a", "b", "c", "d", "e", "f", "g"];
 
-var currentSet = "firstSet"
-document.getElementById("firstSet").style.fontWeight = "bold"
+//Pinyin Lists (+ "_Pinyin"):
+const randomSet_Pinyin = ["wu liao", "gan ga", "gao su", "da suan", "can guan", "wei kou hao", "gan mao", "fan guo lai shuo", "chun xia qiu dong", "sui ran", "zhuo zi", "kai shi"];
+const secondSet_Pinyin = ["ah", "bé", "c'est", "dé", "eux", "effe", "j'ai"];
+
+//Definition Lists (+ "_Definition"):
+const randomSet_Definition = ["ennuyeux", "génant", "avertir/prévenir", "planifier", "visiter", "bon appétit!", "rhume", "d'autre part (connecteur logique)", "printemps, été, automne, hiver", "cependant (connecteur logique)", "table", 'Commencer (appuyez sur "Prochaine Carte")'];
+const secondSet_Definition = ["al", "bl", "cl", "dl", "el", "fl", "gl"];
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+var charList = randomSet.filter(function(f){return f;}); //ensemble principal, change lorsqu'on choisit un autre ensemble.
+var pinyinList = randomSet_Pinyin.filter(function(f){return f;}); //ensemble de pinyin actuel
+var definitionList =  randomSet_Definition.filter(function(f){return f;}); //ensemble de définitions actuelles
+// le filtre est pour transformer la liste en objet de fonction, car si la liste est égal à une autre liste, changer une des deux listes change les deux listes.
+var currentSet = "randomSet"; //met l'ensemble actuel à "randomSet"
 
 //-------------------------------------------------------------------------------------------------------------------------
 
 function replaceCharSet(set) {
-	new File(String, "characterSets.txt")
-	readAsText
+	charList = eval(set).filter(function(f){return f;});
+	pinyinList = eval(set + "_Pinyin").filter(function(f){return f;});
+	definitionList = eval(set + "_Definition").filter(function(f){return f;});
 
-	charList = eval(set);
-	document.getElementById(currentSet).style.fontWeight = "normal" //met le poid du texte en normal
-	document.getElementById(set).style.fontWeight = "bold" //met le poid du texte du nouvel ensemble à lourd
-	currentSet = set
+	document.getElementById(currentSet + "_Element").style.fontWeight = "normal"; //met le poid du texte en normal
+	document.getElementById(set + "_Element").style.fontWeight = "bold"; //met le poid du texte du nouvel ensemble à lourd
+	currentSet = set;
 	console.log("Set charList to", set + ":", charList);
 	generateCard();
 }
@@ -23,10 +37,13 @@ function replaceCharSet(set) {
 
 function generateCard() {
 	var currentDisplayChar = document.getElementById("mainCharacterDisplay").innerHTML; //trouver le caractère actuel
-	charList.splice(charList.indexOf(currentDisplayChar), 1); //enlève le charactère actuel de la liste
+	let currentIndex = charList.indexOf(currentDisplayChar)
+	charList.splice(currentIndex, 1); //enlève le charactère actuel de la liste
+	pinyinList.splice(currentIndex, 1); //enlève le pinyin du caractère actuel de sa liste
+	definitionList.splice(currentIndex, 1); //enlève la définition du caractère actuel de sa liste
 
 	let randomInteger = Math.floor(Math.random() * charList.length); // randint(0, len(charList))
-	console.log("Generated random number:", randomInteger) //affiche la valeur générée dans la console
+	console.log("Generated random number:", randomInteger); //affiche la valeur générée dans la console
 	let generatedChar = charList[randomInteger]; //prend le numéro aléatoire comme index de la liste
 
 	if (charList.length == 0) {
@@ -40,7 +57,7 @@ function generateCard() {
 }
 
 function endCurrentSet() {
-	console.log("charList empty."); //afiche que la variable est vide.
+	console.log("charList empty."); //affiche que la variable est vide.
 	document.getElementById("mainCharacterDisplay").innerHTML = "Fin d'ensemble."; //message de fin
 	document.getElementById("mainPinyinDisplay").style.display = "none"; //cache l'élément texte qui affiche le pinyin
 	document.getElementById("mainDefinitionDisplay").style.display = "none"; //cache l'élément texte qui affiche la définition
@@ -49,12 +66,13 @@ function endCurrentSet() {
 }
 
 function restartSet() {
+	console.log("restart attempt");
 	replaceCharSet(currentSet); //Remet tous les mots dans la liste
 	document.getElementById("nextCardButton").value = "Prochaine Carte"; //remet le texte du bouton à "Prochaine Carte"
 	document.getElementById("nextCardButton").onclick = generateCard; //remet la function du bouton à generateCard()
-	generateCard();
-	document.getElementById("mainPinyinDisplay").style.display = "";
-	document.getElementById("mainDefinitionDisplay").style.display = "";
+	generateCard(); //génère une carte débutante
+	document.getElementById("mainPinyinDisplay").style.display = ""; //affiche les textes
+	document.getElementById("mainDefinitionDisplay").style.display = ""; //affiche les textes
 }
 
 //-------------------------------------------------------------------------------------------------------------------------
