cbc3e7dba82cf4d96455a9a119195d432d709635
Bard-Gaming <christophe@dronne.fr>
2023-01-26 21:10:55 +0100

    Weewoo (chinese flashcards ++)

--- javaScript/flashCards.js
+++ javaScript/flashCards.js
@@ -1,15 +1,21 @@
 //-----------------------------------------------Constantes-------------------------------------------------------------:
 //Character Lists:
-var randomSet = ["无聊", "尴尬", "告诉", "打算", "参观", "胃口好", "感冒", "反过来说", "春夏秋冬", "虽然", "桌子", "开始"];
-const secondSet = ["a", "b", "c", "d", "e", "f", "g"];
+const randomSet = ["无聊", "尴尬", "告诉", "打算", "参观", "胃口好", "感冒", "反过来说", "春夏秋冬", "虽然", "桌子", "开始"];
+const superstitionSet = ["迷信", "避免", "否则", "运气", "随时", "降临", "许多", "使用", "甚至", "直接", "具有", "意义", "肯定", "不幸", "无论", "厄运", "证据", "证明", "忌讳", "在意", "忽视"];
+const legendsSet = ["有害", "既然", "打破", "讲解", "保准", "回答", "创作", "胜", "传说"];
+const citySet = ["新词", "香港", "澳门", "人口", "地理", "由…组成", "公里", "面积", "语信", "气候", "经济", "历史", "政治", "其他", "外国", "简体字", "简单", "离", "土地", "平方公里"];
 
 //Pinyin Lists (+ "_Pinyin"):
 const randomSet_Pinyin = ["wu liao", "gan ga", "gao su", "da suan", "can guan", "wei kou hao", "gan mao", "fan guo lai shuo", "chun xia qiu dong", "sui ran", "zhuo zi", "kai shi"];
-const secondSet_Pinyin = ["ah", "bé", "c'est", "dé", "eux", "effe", "j'ai"];
+const superstitionSet_Pinyin = ["mi xin", "bi mian", "fou ze", "yun qi", "sui shi", "jiang lin", "xu duo", "shi yong", "shen zhi", "zhi jie", "ju you", "yi yi", "ken ding", "bu xing", "wu lun", "e yun", "zheng ju", "zheng ming", "ji hui", "zai yi", "hu shi"];
+const legendsSet_Pinyin = ["you hai", "ji ran", "da po", "jiang jie", "bao zhun", "hui da", "chuang zuo", "sheng", "chuan shuo"];
+const citySet_Pinyin = ["xin ci", "xiang gang", "ao men", "ren kou", "di li", "you...zu cheng", "gong li", "mian ji", "yü xin", "qi hou", "jiang ji", "li shi", "zheng zhi", "qi ta", "wai guo", "jian ti zi", "jian dan", "li", "tu di", "ping fang gong li"];
 
 //Definition Lists (+ "_Definition"):
 const randomSet_Definition = ["ennuyeux", "génant", "avertir/prévenir", "planifier", "visiter", "bon appétit!", "rhume", "d'autre part (connecteur logique)", "printemps, été, automne, hiver", "cependant (connecteur logique)", "table", 'Commencer (appuyez sur "Prochaine Carte")'];
-const secondSet_Definition = ["al", "bl", "cl", "dl", "el", "fl", "gl"];
+const superstitionSet_Definition = ["superstition", "éviter", "sinon", "chance", "à tout moment", "la chance tombe/arrive", "très beaucoup", "utiliser", "voir même jusqu'à/même", "directement", "posséder", "sens", "sans aucun doute", "malheureux", "peu importe", "malheur/malchance", "preuve", "prouver", "taboo", "s'en foutre", "négliger"];
+const legendsSet_Definition = ["endommager", "étant donné que... (connecteur logique)", "casser/briser", "expliquer", "assurer que/être sûre que", "répondre", "inventer/créer [concepte/idée (pas concret)]", "gagner/battre qqn", "légende"];
+const citySet_Definition = ["nouveau mot", "Hong Kong (Ville)", "Macao (Ville)", "Population", "géographie", "à partir de ... se former", "kilomètre (km)", "surface (aire)", "langage/langue", "climat", "économie", "histoire", "la politique", "d'autre", "pays étranger", "caractère simplifié", "simple", "distant (struct: A distant de B [distance entre A et B])", "la terre (pas la planète)", "kilomètre carré (km²)"];
 
 //-------------------------------------------------------------------------------------------------------------------------
 
