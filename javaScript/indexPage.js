9e6ccc493a3b84c56744f7ab4f86cf6b1e707508
efa <aef>
2023-02-02 11:27:46 +0100

    FINALLY GOT index.html TO WORK

--- javaScript/indexPage.js
+++ javaScript/indexPage.js
@@ -1,5 +1,7 @@
-function idk() {
-	alert("hello");
-}
+const supportsNativeSmoothScroll = 'scrollBehavior' in document.documentElement.style;
+console.log(`Native SmoothScroll: ${supportsNativeSmoothScroll}`)
 
-//document.getElementById("mydiv1").scrollIntoView();
+function ownScrollTo(id) {
+	console.log(`Displaying: ${document.getElementById(id)}\nInnerHTML: ${document.getElementById(id).innerHTML}`);
+	document.getElementById(id).scrollIntoView({behavior: "smooth"});
+}
