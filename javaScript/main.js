259b6951394de8741ed7bf9fe2d8e52d6525683b
pajdc <g>
2023-02-06 09:41:35 +0100

    Added class content, almost finished up index, added [Chinese] button dropdown menu

--- javaScript/main.js
+++ javaScript/main.js
@@ -5,13 +5,19 @@ navbarHeader.innerHTML = `
 <ul class="navbarMainList">
 	<li><a id="fpLogo" href="index.html" title="Page d'accueil"><img src="resources/icon.png"></a></li>
 	<li>
-		<a href="htmlPages/programIndex.html">Programmes ↓</i></a>
+		<a href="htmlPages/programIndex.html">Programmes ↓</a>
 		<ul class="programList">
-			<li><a href="htmlPages/programPages/baseConverter.html">Convertisseur de Bases</a></li>
-			<li><a href="htmlPages/programPages/flashCards.html">Cartes Flash</a></li>
+			<li><a class="listElement1" href="htmlPages/programPages/baseConverter.html">Convertisseur de Bases</a></li>
+			<li><a class="listElement1" href="htmlPages/programPages/flashCards.html">Cartes Flash</a></li>
 		</ul>
 	</li>
-	<li><a href="htmlPages/chineseIndex.html">Ressources Chinois</a></li>
+	<li>
+		<a href="htmlPages/chineseIndex.html">Chinois ↓</a>
+		<ul class="programList">
+			<li><a class="listElement2" href="htmlPages/chineseResourcePages/classContent.html">Cours</a></li>
+			<li><a class="listElement2" href="htmlPages/chineseResourcePages/resourceLinks.html">Ressources</a></li>
+		</ul>
+	</li>
 	<li><a href="htmlPages/aboutPage.html">À propos</a></li>
 
 </ul>`;
