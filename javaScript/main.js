052eff6a1b6047f047afe871ecb442373886416a
Bard-Gaming <christophe@dronne.fr>
2023-02-06 19:27:04 +0100

    classContent

--- javaScript/main.js
+++ javaScript/main.js
@@ -25,3 +25,34 @@ navbarHeader.innerHTML = `
 function loadNavbar() {
 	document.body.appendChild(navbarHeader);
 }
+
+//changeTab
+function changeTab(event, contentId) {
+	var i;
+	var consoleString = "";
+	var tabContentList = document.getElementsByClassName("tabContent");
+	var tabButtonList = document.getElementsByClassName("tabButton");
+
+	console.log(event);
+
+	//for element in tabContentList: hide element
+	for (var i = 0; i < tabContentList.length; i++) {
+		consoleString += `, "${tabContentList[i].id}"`;
+		tabContentList[i].style.display = "none";
+	}
+	console.log(`Hid Elements with id: ${consoleString.substring(2)}.`);
+	consoleString = "";
+
+	//enlève la classe "currentButton" de tous les elements dans tabButtonList
+	for (var i = 0; i < tabButtonList.length; i++) {
+		consoleString += `, ${tabButtonList[i].innerHTML}`;
+		tabButtonList[i].classList.remove("currentButton");
+	}
+	console.log(`Removed class "currentButton" from Elements with html: ${consoleString.substring(2)}.`);
+
+	//ajoute classe "currentButton" au bouton + affiche le contenu
+	console.log(`Adding class "currentButton" to Element with html: ${event.srcElement.innerHTML}.`)
+	event.srcElement.classList.add("currentButton");
+	document.getElementById(contentId).style.display = "block";
+
+}
